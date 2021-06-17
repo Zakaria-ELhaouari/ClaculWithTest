78d1bf94e474c62df2f6b0f9171136a34ff3d7f2
Ziko <Ziko@gmail.com>
2021-06-17 23:45:08 +1000

    Add project files.

---
+++ ClaculWithTest/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClaculWithTest
+{
+    public class Add
+    {
+        public int Some(int nm1, int nm2)
+        {
+            var res = nm1 + nm2;
+
+            return res;
+        }
+    }
+}
---
+++ ClaculWithTest/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClaculWithTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitBtn_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Int16.Parse(nm1.Text);
+            var num2 = Int16.Parse(nm2.Text);
+
+            var res  = Add(num1, num2);
+
+            result.Text = res.ToString();
+        }
+
+        public int Add(int nm1 , int nm2)
+        {
+            var res = nm1 + nm2;
+
+            return res;
+        }
+    }
+}
